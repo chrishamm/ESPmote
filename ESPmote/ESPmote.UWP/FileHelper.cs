6137d86d79b572e57b6627877b34eeb986bd0e66
disceris <jackdisceris@gmail.com>
2018-01-18 19:18:15 +0100

    uwp filehelper

---
+++ ESPmote/ESPmote.UWP/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿using ESPmote.UWP;
+using System.IO;
+using Windows.Storage;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace ESPmote.UWP
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            return Path.Combine(ApplicationData.Current.LocalFolder.Path, filename);
+        }
+    }
+}
